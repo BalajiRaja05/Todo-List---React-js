52a16b6d038a59c206d2ce314d85aa5a22a2ab35
BalajiRaja05 <mbalajiraja.21@gmail.com>
2024-02-24 22:14:32 +0530

    first commit todo

--- src/App.js
+++ src/App.js
@@ -5,24 +5,7 @@ import TodoHead from './TodoHead';
 import InputAddItem from './Components/InputAddItem';
 
 function App() {
-  const [items,setItems] =useState(
-    [
-        {
-            id:1,
-            checked :true,
-            content:"Work Hard"
-        },
-        {
-            id:2,
-            checked :false,
-            content:"Keep focus"
-        },
-        {
-            id:3,
-            checked :false,
-            content:"You reach success"
-        }
-])
+  const [items,setItems] =useState(JSON.parse(localStorage.getItem('todo_list')))
 
 const [newItem,setNewItem] = useState('')
 
